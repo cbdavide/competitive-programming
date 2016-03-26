59f09d101dfb0216809b2e57ee0b1719625f93f6
David Castelblanco B <david.castelblanco@hotmail.com>
2016-03-26 10:38:50 -0500

    Added solution of kattis problem aaah, don't judge me I thougth it has some kind of trap.

---
+++ aaah/ahh.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+main() {
+  string a1, a2;
+  cin >> a1 >> a2;
+
+  if(a1.size() >= a2.size())
+    cout << "go" << endl;
+  else
+    cout << "no" << endl;
+}
