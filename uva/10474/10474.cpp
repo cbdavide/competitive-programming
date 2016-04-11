43151ecf9d41ab9de1ae4ecc552b042b10c609a2
David Castelblanco B <david.castelblanco@hotmail.com>
2016-04-10 19:51:34 -0500

    Added solution uva problem 1074 in c++ using the sorting algorithm of the standard library, not fast enough.

--- uva/10474/10474.cpp
+++ uva/10474/10474.cpp
@@ -1,22 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int *arr, int size) {
-    int aux;
-    for(int i=0; i<size; i++) {
-        for(int j=i; j<size; j++) {
-            if(arr[i] > arr[j]) {
-                aux = arr[i];
-                arr[i] = arr[j];
-                arr[j] = aux;
-            }
-        }
-    }
-}
-
-int find(int *arr, int size, int value) {
-    for(int i=0; i<size; i++) {
-        if(arr[i] == value)
+int find(vector<int> arr, int val) {
+    for(int i=0; i<arr.size(); i++) {
+        if(arr[i] == val)
             return i;
     }
     return -1;
@@ -24,17 +11,17 @@ int find(int *arr, int size, int value) {
 
 main() {
     int nmarbles, nquerys, query, index, k=1;
-    int *marbles;
+
     while(cin >> nmarbles >> nquerys && nmarbles && nquerys) {
-        marbles = new int[nmarbles];
+        vector<int>marbles(nmarbles);
         for(int i=0; i<nmarbles; i++) {
             cin >> marbles[i];
         }
-        bubbleSort(marbles, nmarbles);//Array sorted.
+        sort(marbles.begin(), marbles.end());//Array sorted.
         cout << "CASE# " << k++ << ":\n";
         for(int i=0; i<nquerys; i++) {
             cin >> query;
-            index = find(marbles, nmarbles, query);
+            index = find(marbles, query);
             if(index != -1) {
                 cout << query << " found at " << index+1 << '\n';
             } else {
