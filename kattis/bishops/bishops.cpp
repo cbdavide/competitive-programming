b7da2ed318b5a706a3dcbf66ffd1d4112d7befab
David Castelblanco B <david.castelblanco@hotmail.com>
2016-05-25 14:29:00 -0500

    Added solution kattis problem bishops

---
+++ kattis/bishops/bishops.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    while(cin >> n) {
+        if((2*n) - 2)
+            cout << (2*n) - 2 << '\n';
+        else
+            cout << "1\n";
+    }
+}
