a5c1028156322578f0bd2d51b27ec7ecedd3acf0
David Castelblanco Beanvides <cbdavides@gmail.com>
2017-08-06 19:22:59 -0500

    Fixed the solution of the kattis problem bing (Trie)

--- kattis/bing/bing.cpp
+++ kattis/bing/bing.cpp
@@ -1,28 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+struct node {
+    node* children[26];
+    int prefix, words;
+    bool is_end;
 
-    int n, cont;
+    node() {
+        memset(children, 0, sizeof(children));
+        is_end = false;
+        prefix = words = 0;
+    }
+};
+
+void insert(node *nd, string::iterator s) {
+    if(*s) {
+        nd->prefix += 1;
+        if(!nd->children[*s - 'a'])
+            nd->children[*s - 'a'] = new node();
+        insert(nd->children[*s - 'a'], s + 1);
+    } else {
+        nd->is_end = true;
+        nd->words += 1;
+    }
+}
+
+int countWords(node *nd, string::iterator s) {
+    if(*s) {
+        if(!nd->children[*s - 'a'])
+            return 0;
+        return countWords(nd->children[*s - 'a'], s + 1);
+    } else {
+        return nd->words;
+    }
+}
+
+int countPre(node *nd, string::iterator s) {
+    if(*s) {
+        if(!nd->children[*s - 'a'])
+            return 0;
+
+        return countPre(nd->children[*s - 'a'], s + 1);
+    } else {
+        return nd->prefix;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, cont, r;
     string *words, temp;
 
     while(cin >> n) {
         words = new string[n];
+        node *trie = new node();
 
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) {
             cin >> words[i];
-
-        for(int i=0; i<n; i++){
-            cont = 0;
-
-            for(int j=i-1; j>=0; j--) {
-
-                if(words[i] == words[j].substr(0, words[i].size()))
-                    cont++;
-            }
-            cout << cont << '\n';
+            r = countPre(trie, words[i].begin()) + countWords(trie, words[i].begin());
+            cout << r << '\n';
+            insert(trie, words[i].begin());
         }
     }
-
     return 0;
 }
