f59b410f59d8cac9b9a31533331ffe6551d098d6
David Castelblanco Beanvides <cbdavides@gmail.com>
2017-10-19 23:26:04 -0500

    Added solution to UVa problem 10041 (Complete Search)

---
+++ uva/10041/10041.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class T> int size(const T &x) {return x.size();}
+template <class T> T mod(T a, T b) { return (b + (a % b)) % b;}
+
+#define F first
+#define S second
+#define endl '\n'
+#define rep(i, a, b) for(__typeof(a) i=a; i<(b); i++)
+#define iter(it, c) for(__typeof((c).begin()) it=(c).begin(); \
+ it != (c).end(); it++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef set<int> si;
+
+const int INF = ~(1 << 31);
+const double EPS = 1e-9;
+const double PI = acos(-1);
+
+int main() {
+    int n, k, curr, cont;
+    cin >> n;
+    while(n--) {
+        cin >> k; vi nums(k);
+        rep(i, 0, k) cin >> nums[i];
+        cont = INF;
+        rep(i,0, k) {
+            curr = 0;
+            rep(j, 0, k) {
+                curr += abs(nums[i] - nums[j]);
+            }
+            cont = min(curr, cont);
+        }
+        cout << cont << endl;
+    }
+    return 0;
+}
